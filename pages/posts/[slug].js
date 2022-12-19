57eb8a423c85b1f7dece0d8205660c64750b0382
[mikimu] <[mikimuuu@gmail.com]>
2022-12-20 01:07:13 +0900

    ittAnkore

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,36 +1,25 @@
-import fs from 'fs'
-import matter from 'gray-matter'
-import {marked} from 'marked'
+import fs from 'fs';
 
-export async function getStaticProps({ params }) {
-    const file = fs.readFileSync(`posts/${params.slug}.md`, 'utf-8');
-    const { data, content } = matter(file);
-    return { props: { frontMatter: data, content } };
-  }
-
-  export async function getStaticPaths() {
-    const files = fs.readdirSync('posts');
-    const paths = files.map((fileName) => ({
-      params: {
-        slug: fileName.replace(/\.md$/, ''),
-      },
-    }));
-    console.log('paths:', paths);
-    return {
-      paths,
-      fallback: false,
-    };
-  }
-
-
-const Post = ({frontMatter,content}) => {
-    return (
-        <div>
-           <h1>{frontMatter.title}</h1>
-            <div dangerouslySetInnerHTML=
-            {{__html:marked(content)}}></div>        
-        </div>
-    )
+export async function getStaticProps() {
+  return { props: { post: '' } };
 }
 
-export default Post
+export async function getStaticPaths() {
+  const files = fs.readdirSync('posts');
+  const paths = files.map((fileName) => ({
+    params: {
+      slug: fileName.replace(/\.md$/, ''),
+    },
+  }));
+  console.log('paths:', paths);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const Post = () => {
+  return <div>コンテンツ</div>;
+};
+
+export default Post;
