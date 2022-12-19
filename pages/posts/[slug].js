3d7e621e45fe71df4f76e7dc98b1f9c419156676
[mikimu] <[mikimuuu@gmail.com]>
2022-12-19 22:56:37 +0900

    tanomu

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,34 @@
-const Post = () => {
-    return <div>コンテンツ</div>;
+import fs from 'fs';
+import matter from 'gray-matter';
+
+export async function getStaticPaths() {
+    const files = fs.readdirSync('posts');
+    const paths = files.map((fileName) => ({
+      params: {
+        slug: fileName.replace(/\.md$/, ''),
+      },
+    }));
+    console.log('paths:', paths);
+    return {
+      paths,
+      fallback: false,
+    };
+  }
+
+export async function getStaticProps({ params }) {
+  const file = fs.readFileSync(`posts/${params.slug}.md`, 'utf-8');
+  const { data, content } = matter(file);
+  return { props: { frontMatter: data, content } };
+}
+
+const Post = ({ frontMatter, content }) => {
+    return (
+      <div>
+        <h1>{frontMatter.title}</h1>
+        <div>{content}</div>
+      </div>
+    );
   };
   
   export default Post;
+  
