3fb71eae842150ea889f8a29e295f88792de85c8
[mikimu] <[mikimuuu@gmail.com]>
2022-12-19 22:53:56 +0900

    ittankore

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,32 +1,5 @@
-import fs from 'fs'
-import matter from 'gray-matter'
-
-export async function getStaticProps({ params: { slug } }) {
-    const file = fs.readFileSync('posts/${params.slug}.md', 'utf-8')
-    const { data, content } = matter(file)
-    return {props:{frontMatter:data,content} }
-}
-
-export async function getStaticPaths() {
-    const files = fs.readdirSync('posts')
-    const paths = files.map(filename => ({
-        params: {
-            slug: filename.replace(/\.md$/, '')
-        }
-    }))
-    return {
-        paths,
-        fallback: false
-    }
-}
-
-const Post = ({ frontMatter, content }) => {
-    return (
-        <div>
-            <h1>{frontMatter.title}</h1>
-            <div>{content}</div>
-        </div>
-    )
-}
-
-export default Post
+const Post = () => {
+    return <div>コンテンツ</div>;
+  };
+  
+  export default Post;
